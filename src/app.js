b24ca1b3a56a0051865348d954920159ab265b2a
Mayara Roseno <mayararoseno14@gmail.com>
2026-03-08 22:36:06 -0300

    feat: create order model

--- src/app.js
+++ src/app.js
@@ -1,12 +1,15 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
+const orderRoutes = require("./routes/orderRoutes");
 
 const app = express();
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("API Jitterbit funcionando");
-});
+mongoose.connect("mongodb://localhost:27017/orders");
+
+app.use("/order", orderRoutes);
 
 app.listen(3000, () => {
   console.log("Servidor rodando na porta 3000");
