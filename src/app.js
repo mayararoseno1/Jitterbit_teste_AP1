9a323b8ef192d823fdeed973baaba825e27a50a7
Mayara Roseno <mayararoseno14@gmail.com>
2026-03-08 22:32:43 -0300

    feat: create initial express server

---
+++ src/app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("API Jitterbit funcionando");
+});
+
+app.listen(3000, () => {
+  console.log("Servidor rodando na porta 3000");
+});
